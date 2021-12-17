19bd6afbd9c3508d88b7b3af494da2af557322a0
Andreas Amann <a.amann@ucc.ie>
2021-12-17 17:11:26 +0000

    Improve build system

---
+++ INSTALL.md
@@ -0,0 +1,7 @@
+# Install from Source
+
+```bash
+git clone https://github.com/andram/nonlindyn.git
+cd nonlindyn
+python -m pip install --editable . --user
+```
--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 setup(name='nonlindyn',
-      version='0.1',
+      version='0.2',
       description='Library to support analysing nonlinear dynamical systems',
       url='http://github.com/andram/nonlindyn',
       author='Andreas Amann',
       author_email='a.amann@ucc.ie',
       license='GPL',
       packages=['nonlindyn'],
-      zip_safe=False)
+)
