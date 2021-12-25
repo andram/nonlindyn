8a6e6dd58bcf85250a24c83b027fcac21d852812
Andreas Amann <a.amann@ucc.ie>
2021-12-25 01:07:00 +0000

    local extrema

--- nonlindyn/extrema.py
+++ nonlindyn/extrema.py
@@ -1,18 +1,22 @@
 from collections.abc import Iterator, Generator
 import numpy as np
 
-def filter_max(
-        generator: Iterator[tuple[float,np.ndarray]],
-        index: int = 0
-) -> Generator[tuple[float,np.ndarray], None, None]:
-    tx_old = next(generator)
+def local_max(generator, key = None):
+    key = (lambda x:x) if key is None else key
+    generator = iter(generator)
+    tx_prev = next(generator)
     tx = next(generator)
     for tx_next in generator:
-        if (
-                tx_old[1][index] < tx[1][index]
-                and tx[1][index] >= tx_next[1][index]
-        ): # local maximum
-            # TODO: proper interpolation
+        if ( key(tx_prev) < key(tx)  >= key(tx_next) ): 
             yield tx
-        tx_old = tx
-        tx = tx_next
+        tx_prev, tx = tx, tx_next
+
+def local_min(generator, key = None):
+    key = (lambda x:x) if key is None else key
+    generator = iter(generator)
+    tx_prev = next(generator)
+    tx = next(generator)
+    for tx_next in generator:
+        if ( key(tx_prev) > key(tx)  <= key(tx_next) ): 
+            yield tx
+        tx_prev, tx = tx, tx_next
