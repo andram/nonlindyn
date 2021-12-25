a441b87afb95ad46a006c5cf92cb143bef7dd1e9
Andreas Amann <a.amann@ucc.ie>
2021-12-25 16:30:34 +0000

    simplify minima

--- nonlindyn/extrema.py
+++ nonlindyn/extrema.py
@@ -1,22 +1,22 @@
 from collections.abc import Iterator, Generator
-import numpy as np
+import itertools as it
 
 def local_max(generator, key = None):
+    """
+    Calculate the local maximum
+    """
     key = (lambda x:x) if key is None else key
     generator = iter(generator)
-    tx_prev = next(generator)
-    tx = next(generator)
+    tx_prev, tx = next(generator), next(generator) 
     for tx_next in generator:
         if ( key(tx_prev) < key(tx)  >= key(tx_next) ): 
             yield tx
         tx_prev, tx = tx, tx_next
-
+        
 def local_min(generator, key = None):
-    key = (lambda x:x) if key is None else key
-    generator = iter(generator)
-    tx_prev = next(generator)
-    tx = next(generator)
-    for tx_next in generator:
-        if ( key(tx_prev) > key(tx)  <= key(tx_next) ): 
-            yield tx
-        tx_prev, tx = tx, tx_next
+    """
+    Calculate the local minimum
+    """
+    # Let's just re-use the local maximum function with inverted logic. 
+    key_inv = (lambda x:-x) if key is None else (lambda x: -key(x))
+    return local_max(generator, key=key_inv)
